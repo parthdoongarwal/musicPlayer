777d711ea4fbfcf86c9b0cef9ef48844255115ea
Parth Doongarwal <77840382+parthdoongarwal@users.noreply.github.com>
2023-08-08 15:58:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,27 +8,32 @@ let count = 0;
 
 const data = [
   {
-    img: "/images/Cindrella.jpg",
-    name: "Full『Cinderella』by",
-    url: "[MP3FY] Komi-san Can't Communicate - Opening Full『Cinderella』by Cidergirl.mp3",
+    img: "/Cindrella.jpg",
+    name: "Full『Cinderella",
+    url: "/[MP3FY] Komi-san Can't Communicate - Opening Full『Cinderella』by Cidergirl.mp3",
   },
 
   {
-    img: "/images/Cradles.jpg",
+    img: "/Cradles.jpg",
     name: "Cradles",
-    url: "[MP3FY] Sub Urban - Cradles [Official Music Video].mp3",
+    url: "/[MP3FY] Sub Urban - Cradles [Official Music Video].mp3",
   },
 
   {
-    img: "/images/ErasedEn.jpg",
+    img: "/ErasedEn.jpg",
     name: "Erased ending song",
-    url: "[MP3FY] Sayuri - Sore wa Shiisana Hikari no Youna _ Boku Dake Ga Inai Machi (ERASED) FULL ENDING.mp3",
+    url: "/[MP3FY] Sayuri - Sore wa Shiisana Hikari no Youna _ Boku Dake Ga Inai Machi (ERASED) FULL ENDING.mp3",
   },
 
   {
     img: "https://i.ytimg.com/vi/foKNT2tLfl8/hqdefault.jpg",
-    url: "[MP3FY] FullMetal Alchemist Brotherhood ED 1 (1).mp3",
+    url: "/[MP3FY] FullMetal Alchemist Brotherhood ED 1 (1).mp3",
     name: "Full metal Brotherhood",
+  },
+  {
+    img: "https://s.yimg.com/ny/api/res/1.2/bj.C.kc8dq5eHOFW4W4PwQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTY0MA--/https://media.zenfs.com/en/spin_digital_media/6bb28b3a3d14ca8b87df83dea3e1d3d3",
+    url: `/XXXTENTACION_-_Jocelyn_Flores_Audio[ConverteZilla.com].mp3`,
+    name: "xxxtentacion",
   },
 ];
 
